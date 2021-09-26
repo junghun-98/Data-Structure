2fe2ea28313df2dd1b84fafd8c4f2720b734f586
junghun-98 <cjswpwjdgns@naver.com>
2021-09-26 15:37:57 +0900

    Implement list toolkit

--- The bag class with a linked list/DoublyLinkedLIst.cpp
+++ The bag class with a linked list/DoublyLinkedLIst.cpp
@@ -1,6 +1,87 @@
+//dnode Class를 사용한 함수(list류 함수들) 구현 파일
+//bag Class의 멤버 함수 구현 파일
+
 #include "DoublyLinkedLIst.h"
 
-int main() {
-	cout << "Hello world!";
-	return 0;
+size_t list_length(const dnode* head_ptr){
+	size_t size = 0;
+	const dnode* cursor = head_ptr;
+	
+	while (cursor != NULL) {
+		cursor = cursor->get_forelink();
+		size++;
+	}
+	return size;
+}
+
+void list_head_insert(dnode*& head_ptr, const dnode::value_type& entry) {
+	head_ptr = new dnode(entry, head_ptr, NULL);
+}
+
+void list_insert(dnode* previous_ptr, const dnode::value_type& entry) {
+	dnode* insert_ptr = new dnode(entry, previous_ptr->get_forelink(), previous_ptr);
+	previous_ptr->set_forelink(insert_ptr);
+}
+
+const dnode* list_search(const dnode* head_ptr, const dnode::value_type& target) {
+	const dnode* cursor = head_ptr;
+
+	while (cursor != NULL)
+	{
+		if (cursor->get_data() == target)
+			return cursor;
+	}
+	return NULL;
+}
+
+const dnode* list_locate(const dnode* head_ptr, size_t position) { // precondition: position > 0
+	const dnode* cursor = head_ptr;
+	assert(position > 0);
+	
+	for (size_t i = 0; cursor != NULL && i < position; i++)
+	{
+		cursor = cursor->get_forelink();
+	}
+	return cursor;
+}
+
+void list_copy(const dnode* source_ptr, dnode*& head_ptr, dnode*& tail_ptr) {
+	head_ptr = NULL, tail_ptr = NULL;
+
+	if (source_ptr == NULL) return;
+	
+	list_head_insert(head_ptr, source_ptr->get_data());
+	
+	tail_ptr = head_ptr;
+	source_ptr = source_ptr->get_forelink();
+	
+	while (source_ptr != NULL) {
+		list_insert(tail_ptr, source_ptr->get_data());
+		
+		tail_ptr = tail_ptr->get_forelink();
+		source_ptr = source_ptr->get_forelink();
+	}
+}
+
+void list_head_remove(dnode*& head_ptr) {
+	dnode* remove_ptr = head_ptr;
+	
+	head_ptr = head_ptr->get_forelink();
+	head_ptr->set_backlink(NULL);
+	
+	delete remove_ptr;
+}
+
+void list_remove(dnode* previous_ptr) {
+	dnode* remove_ptr = previous_ptr->get_forelink();
+	
+	previous_ptr->set_forelink(remove_ptr->get_forelink());
+	
+	delete remove_ptr;
+}
+
+void list_clear(dnode*& head_ptr) {
+	while (head_ptr != NULL) {
+		list_head_remove(head_ptr);
+	}
 }
--- The bag class with a linked list/DoublyLinkedLIst.h
+++ The bag class with a linked list/DoublyLinkedLIst.h
@@ -1,32 +1,33 @@
 #pragma once
 #include <iostream>
+#include <cassert>
 
 using namespace std;
 
-/*Doubly Linked List를 이루는 기본 단위 class dnod */
+/*Doubly Linked List를 이루는 기본 단위 dnod */
 class dnode {
 public:
 	typedef double value_type;
 
-	dnode() {
-		//Data initialize
-		data_field = value_type();
-		link_fore = NULL;
-		link_back = NULL;
+	dnode(const value_type& data = value_type(), dnode* init_forelink = NULL, dnode* init_backlink = NULL ) {
+		//dnode initialize
+		data_field = data;
+		link_fore = init_forelink;
+		link_back = init_backlink;
 	}
+
 	void set_data(const value_type& new_data) { data_field = new_data; }
 	
 	void set_forelink(dnode* new_link) { link_fore = new_link; }
 	void set_backlink(dnode* new_link) { link_back = new_link; }
 
-	value_type data() const { return data_field; }
+	value_type get_data() const { return data_field; }
 	
 	const dnode* get_forelink() const { return link_fore; }
 	dnode* get_forelink() { return link_fore; }
 	
 	const dnode* get_backlink() const { return link_fore; }
 	dnode* get_backlink() { return link_back; }
-
 private:
 	value_type data_field;
 	dnode* link_fore;
@@ -38,10 +39,8 @@ size_t list_length(const dnode* head_ptr);
 void list_head_insert(dnode*& head_ptr, const dnode::value_type& entry);
 void list_insert(dnode* previous_ptr, const dnode::value_type& entry);
 
-dnode* list_search(const dnode* head_ptr, const dnode::value_type& target);
 const dnode* list_search(const dnode* head_ptr, const dnode::value_type& target);
 
-dnode* list_locate(dnode* head_ptr, size_t position);
 const dnode* list_locate(const dnode* head_ptr, size_t position);
 // precondition: position > 0
 
@@ -54,19 +53,26 @@ void list_clear(dnode*& head_ptr);
 
 class bag {
 public:
-	typedef std::size_t size_type;
+	typedef size_t size_type;
 	typedef double value_type;
+
 	bag();
 	bag(const bag& source);
 	~bag();
 	
 	void operator =(const bag& source);
 	void operator +=(const bag& addend);
+	bool operator ==(const bag& source);
+	bool operator !=(const bag& source);
 
 	bool erase_one(const value_type& target);
+
 	size_type count(const value_type& target) const;
 	value_type grab();
+
+	void show_content();
 	void insert(const value_type& entry);
+	void sort();
 private:
 	dnode* head_ptr;
 	size_type many_nodes;
